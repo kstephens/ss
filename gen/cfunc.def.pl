1ea7bf04318866b9d1cc439df9577a1c2bea2439
Kurt Stephens <ks.github@kurtstephens.com>
2014-02-10 23:09:57 -0600

    Ignore incorrectly parsed inline decls.

--- gen/cfunc.def.pl
+++ gen/cfunc.def.pl
@@ -3,14 +3,14 @@
 while ( <> ) {
   chomp;
   s/^extern\s+//;
-  s/^(inline,__inline)\s+//;
+  s/^(inline|__inline)\s+//;
   s/^([a-z0-9_]+)\s+(([*]\s*)+)/$1$2 /i;
   # print STDERR $_, "\n" if ( /fopen/ );
   if ( /^([a-z0-9_]{2,}[*]*)\s+([a-z0-9_]{3,})\s*(\([^)]*\)?)/i ) {
     $type = $1;
     $func = $2;
     $args = $3;
-    next if ( $type =~ /^(typedef|__inline)$/ );
+    next if ( $type =~ /^(typedef|__inline|inline)$/ );
     next if ( $func =~ /^(__typeof__|alloca|GC_win32_free_heap|zopen)$/ );
     next if ( /^(ss_prim|ss_syntax|ss_end)/ );
     if ( 0 ) {
