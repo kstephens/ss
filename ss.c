c9c3c76d26bd64cda7d3a743a77acb6a4d227ae5
Kurt Stephens <ks.github@kurtstephens.com>
2014-02-12 10:54:25 -0600

    ss_init_prim uses init table.

--- ss.c
+++ ss.c
@@ -1315,14 +1315,27 @@ void ss_init_const(ss_s_env *ss_env)
 
 void ss_init_prim(ss_s_env *ss_env)
 {
-  ss sym;
-#define ss_prim_def(NAME,MINARGS,MAXARGS,NO_SIDE_EFFECT,DOCSTRING)      \
-  sym = ss_sym(NAME);                                                   \
-  ss_PASTE2(ss_p_,NAME) =                                               \
-    ss_alloc_copy(ss_t_prim, sizeof(ss_s_prim), &ss_PASTE2(_ss_p_,NAME)); \
-  ss_define(ss_env, sym, ss_PASTE2(ss_p_,NAME));                        \
-  ss_UNBOX(symbol, sym).is_const = 1;
+  static struct {
+    const char *name;
+    int minargs, maxargs, no_side_effect;
+    const char *desc;
+    void *prim_struct;
+    ss *primp;
+  } inits[] = {
+#define ss_prim_def(NAME,MINARGS,MAXARGS,NO_SIDE_EFFECT,DOCSTRING) \
+    { #NAME, MINARGS, MAXARGS, NO_SIDE_EFFECT, DOCSTRING, &ss_PASTE2(_ss_p_,NAME), &ss_PASTE2(ss_p_,NAME) },
 #include "prim.def"
+    { 0 }
+  };
+  ss sym;
+
+  for ( int i = 0; inits[i].name; ++ i ) {
+    ss prim = ss_alloc_copy(ss_t_prim, sizeof(ss_s_prim), inits[i].prim_struct);
+    *inits[i].primp = prim;
+    sym = ss_box_symbol(inits[i].name);
+    ss_define(ss_env, sym, prim);
+    ss_UNBOX(symbol, sym).is_const = 1;
+  }
 
 #define ss_syntax_def(NAME,MINARGS,MAXARGS,NO_SIDE_EFFFECT,DOCSTRING)     \
   sym = ss_sym(NAME);                                           \
