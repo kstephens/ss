92109b4b6687f786df233239aceb458676dfc790
Kurt Stephens <ks.github@kurtstephens.com>
2014-02-13 22:05:30 -0600

    C EOF == -1, therefore ss_eos == ss_c(-1).

--- t/test-char.scm
+++ t/test-char.scm
@@ -1,6 +1,11 @@
 (test eq? (char->integer #\A) 65)
 (test eq? (integer->char 48) #\0)
 
+(test eq? (char->integer (integer->char 256)) 0)
+;; eos <=> -1
+(test eq? (integer->char -1) %eos)
+(test eq? (char->integer (integer->char -1)) -1)
+
 (test eq? (C:ss_c (C:toupper (C:ss_C #\a))) #\A)
 (test eq? (C:ss_c (C:toupper (C:ss_C #\A))) #\A)
 (test eq? (C:ss_c (C:tolower (C:ss_C #\A))) #\a)
