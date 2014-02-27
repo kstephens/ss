8596871f0bcb26e2787bca5ca4c78a96ea501356
Kurt Stephens <ks.github@kurtstephens.com>
2014-02-27 08:09:06 -0600

    More (char) -1 => #<eos> tests.

--- t/test-char.scm
+++ t/test-char.scm
@@ -2,9 +2,13 @@
 (test eq? (integer->char 48) #\0)
 
 (test eq? (char->integer (integer->char 256)) 0)
+(test eq? (char->integer (integer->char 255)) 255)
 ;; eos <=> -1
 (test eq? (integer->char -1) %eos)
+(test eq? (char->integer %eos) -1)
 (test eq? (char->integer (integer->char -1)) -1)
+(test eq? (char->integer (integer->char -2)) -2)
+(test not-eq? (%type (integer->char -2)) %<char>)
 
 (test eq? (C:ss_c (C:toupper (C:ss_C #\a))) #\A)
 (test eq? (C:ss_c (C:toupper (C:ss_C #\A))) #\A)
