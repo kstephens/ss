d320f0c656e1f5b5851723cc9d0b02f61bdc8fd6
Kurt Stephens <ks.github@kurtstephens.com>
2014-02-28 22:48:17 -0600

    Drastically improve ctypes.def.

--- src/cfunc_call.h
+++ src/cfunc_call.h
@@ -11,6 +11,10 @@
     R(((T(*)(T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3])));
   case 5:
     R(((T(*)(T,T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3]), A(ss_argv[4])));
+  case 6:
+    R(((T(*)(T,T,T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3]), A(ss_argv[4]), A(ss_argv[5])));
+  case 7:
+    R(((T(*)(T,T,T,T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3]), A(ss_argv[4]), A(ss_argv[5]), A(ss_argv[6])));
   default: abort();
   }
 #undef T
