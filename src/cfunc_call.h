c37119e1338cb4402fb39a113fa26714277013a3
Kurt Stephens <ks.github@kurtstephens.com>
2014-03-03 03:20:25 -0600

    More cfunc args.

--- src/cfunc_call.h
+++ src/cfunc_call.h
@@ -15,7 +15,11 @@
     R(((T(*)(T,T,T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3]), A(ss_argv[4]), A(ss_argv[5])));
   case 7:
     R(((T(*)(T,T,T,T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3]), A(ss_argv[4]), A(ss_argv[5]), A(ss_argv[6])));
-  default: abort();
+  case 8:
+    R(((T(*)(T,T,T,T,T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3]), A(ss_argv[4]), A(ss_argv[5]), A(ss_argv[6]), A(ss_argv[7])));
+  case 9:
+    R(((T(*)(T,T,T,T,T,T,T,T,T)) ss_prim->c_func)(A(ss_argv[0]), A(ss_argv[1]), A(ss_argv[2]), A(ss_argv[3]), A(ss_argv[4]), A(ss_argv[5]), A(ss_argv[6]), A(ss_argv[7]), A(ss_argv[8])));
+  default: _ss_max_args_error(ss_env, ss_prim, ss_prim->docstring, ss_argc, 7); abort();
   }
 #undef T
 #undef A
