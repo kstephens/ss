fa6ce838960cd57abf0458adee72f0377f46eba9
Kurt Stephens <ks.github@kurtstephens.com>
2014-03-03 20:08:20 -0600

    Cleanup.

--- src/repl.c
+++ src/repl.c
@@ -13,26 +13,17 @@ ss ss_repl(ss_s_env *ss_env, ss input, ss output, ss prompt, ss trap_error)
     ss_CATCH(catch) {
       if ( trap_error != ss_f ) ss_env->error_catch = catch;
       if ( (expr = ss_prompt(ss_env, input, prompt)) == ss_eos ) goto stop;
-
       if ( 0 && prompt != ss_f ) {
         fprintf(*ss_stderr, "  ;; ss: read => "); ss_write(expr, ss_stderr); fprintf(*ss_stderr, "\n");
       }
-
       value = ss_undef;
       value = ss_eval(expr);
-
       if ( 0 && prompt != ss_f ) {
         fprintf(*ss_stderr, "  ;; ss: rewrite => "); ss_write(expr, ss_stderr); fprintf(*ss_stderr, "\n");
       }
       if ( value != ss_undef && value != ss_unspec ) {
         if ( output != ss_f ) {
           ss_write(value, ss_stdout); fprintf(*ss_stdout, "\n");
-        }
-        if ( 0 ) {
-          fprintf(*ss_stderr, "  ;; ss: %lld (%p)\n", (long long) ss_unb(fixnum, value), (void*) value);
-          fprintf(*ss_stderr, "  ;; ss: %llu bytes %llu objects\n",
-                  (unsigned long long) ss_malloc_bytes,
-                  (unsigned long long) ss_malloc_objects);
         }
       }
     }
